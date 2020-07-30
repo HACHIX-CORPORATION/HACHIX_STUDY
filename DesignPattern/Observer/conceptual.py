8b6329e121c0fe37c8cf39f440080320cd3b9b02
chuoru <tranvietthanh0101@gmail.com>
2020-07-30 19:54:56 +0900

    #1 add decorator study

---
+++ DesignPattern/Observer/conceptual.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+"""
+
+Abstract::
+    - 
+History::
+    - Ver.      Date            Author        History
+    - 
+Copyright (C) 2020 HACHIX Corporation. All Rights Reserved.
+"""
+# 標準ライブラリ
+
+
+# 関連外部ライブラリ
+
+
+# 内部ライブラリ
