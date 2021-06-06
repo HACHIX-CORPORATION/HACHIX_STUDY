351550eadded13796ecb751f537d2edf9b94aed8
makotovnjp <makotovnjp@gmail.com>
2021-06-06 13:15:53 +0900

    update

--- Python/algo/27_hash_table_quiz_pair/thanh_main.py
+++ Python/algo/27_hash_table_quiz_pair/thanh_main.py
@@ -15,11 +15,27 @@ def get_pair(numbers: List[int], target: int) -> Optional[Tuple[int, int]]:
     return None
 
 
+def get_pair_half_sum(numbers: List[int]) -> Optional[Tuple[int, int]]:
+    cache = set()
+    total = sum(numbers)
+
+    if total % 2 == 1:
+        return None
+    half = int(total / 2)
+
+    for number in numbers:
+        rest = half - number
+        if rest in cache:
+            return number, rest
+        cache.add(number)
+
+    return None
+
 
 if __name__ == '__main__':
-    l = [11, 2, 5, 9, 11, 3]
-    print(get_pair(l, t))
+    l = [11, 2, 5, 9, 10, 3]
     t = 12
+    print(get_pair(l, t))
 
-    # l = [11, 2, 5, 9, 11, 3]
-    # print(get_pair_half_sum(l))
+    l = [11, 2, 5, 9, 10, 3]
+    print(get_pair_half_sum(l))
