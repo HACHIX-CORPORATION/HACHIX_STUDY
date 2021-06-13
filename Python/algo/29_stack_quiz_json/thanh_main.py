0947c1d1557b904e2ac23fb9eefceb3c6cb5e106
makotovnjp <makotovnjp@gmail.com>
2021-06-13 10:33:27 +0900

    update

--- Python/algo/29_stack_quiz_json/thanh_main.py
+++ Python/algo/29_stack_quiz_json/thanh_main.py
@@ -5,7 +5,26 @@ Input [{'key1': 'value1', 'key2': [1, 2, 3], 'key3': (1, 2, 3)} Output False
 
 
 def validate_format(chars: str) -> bool:
-    return False
+    syn_tags = {
+        '{': '}',
+        '(': ')',
+        '[': ']',
+    }
+    stack = []
+
+    for element in chars:
+        if element in syn_tags.keys():
+            stack.append(syn_tags[element])
+
+        if element in syn_tags.values():
+            if not stack:
+                return False
+            if element != stack.pop():
+                return False
+
+    if stack:
+        return False
+    return True
 
 
 if __name__ == '__main__':
