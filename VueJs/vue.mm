371727e85a099ead37aaeeed93d4b9a1470409f8
makotovnjp <makotovnjp@gmail.com>
2021-07-01 21:51:53 +0900

    update

---
+++ VueJs/vue.mm
@@ -0,0 +1,74 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1625142762511" ID="ID_488213110" MODIFIED="1625142771475" TEXT="VueJs">
+<node CREATED="1625142796332" ID="ID_1853021502" MODIFIED="1625142801830" POSITION="right" TEXT="Why Vue Js">
+<node CREATED="1625142803234" ID="ID_1346412026" MODIFIED="1625143660034" TEXT="Comparison">
+<node CREATED="1625142807754" ID="ID_1913100986" MODIFIED="1625142812903" TEXT="Angular JS">
+<node CREATED="1625142989948" ID="ID_161187439" MODIFIED="1625143288636">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        2010 by google
+      </li>
+      <li>
+        framework: everything built in
+      </li>
+      <li>
+        good for big company: d&#7877; th&#7889;ng nh&#7845;t rule
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1625142813355" ID="ID_1421745622" MODIFIED="1625142817031" TEXT="React JS">
+<node CREATED="1625142869635" ID="ID_544578184" MODIFIED="1625143301051">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        2013 facebook
+      </li>
+      <li>
+        library
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1625142817626" ID="ID_582990219" MODIFIED="1625142820806" TEXT="Vue JS">
+<node CREATED="1625142927371" ID="ID_264504117" MODIFIED="1625143337731">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2014 by google employee
+    </p>
+    <p>
+      mix angluar and react
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1625143661577" ID="ID_702941446" MODIFIED="1625143668757" TEXT="History and Version">
+<node CREATED="1625143834427" ID="ID_1119521012" MODIFIED="1625143848526" TEXT="2020: ver3.0"/>
+<node CREATED="1625143849944" ID="ID_168518327" MODIFIED="1625143855828" TEXT="2019: ver2.6"/>
+</node>
+</node>
+</node>
+</map>
